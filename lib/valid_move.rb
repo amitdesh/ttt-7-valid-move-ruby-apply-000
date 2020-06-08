7808019abda77321a64870a839570288b7393f4e
A D <amitdesh2014@gmail.com>
2020-06-08 16:35:05 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,7 +12,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken? == false && (0<= index <=8)
+  if position_taken? == true && (0<= index <=8)
     return true
   else
     return false
